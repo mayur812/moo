20ca7cde7eb4f7b75b2724308820fd719f9bb17e
Mayur.Joshi <ayuroshi56@gmail.com>
2024-05-22 05:34:37 +0530

    Making Login API

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { error } from 'console';
+import { LoginService } from 'src/app/services/login.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login',
@@ -8,15 +12,41 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   hide = true;
-  user={
+  loginData={
     username:'',
     password:''
   };
+  hasSpecialChars(str:any) {
+    const specialChars = '!@#$%^&*()_+\-=\[\]{};';
+    for (let char of str) {
+      if (specialChars.indexOf(char) !== -1) {
+        return true;
+      }
+    }
+    return false;
+  }
 
   formSubmit(){
+    if (this.loginData.username.trim() == '' || this.loginData.username == null) {
+      this.snack.open("Invalid Username",'',{duration:3000});
+      return
+    }
+    
+    if (this.loginData.password.length < 8 || !this.hasSpecialChars(this.loginData.password)) {
+      this.snack.open("Password does not follow the guidlines!",'Ok')
+      return
+    }
+    this.loginService.generateToken(this.loginData).subscribe(
+      (data:any) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error)
+      }
+    );
     
   }
-  constructor() { }
+  constructor(private snack:MatSnackBar,public loginService:LoginService) { }
 
   ngOnInit() {
   }
